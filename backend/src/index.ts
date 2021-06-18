b9accfff864b5d799b3282f36a073ca356c1e429
Carlos Eustáquio <caeu227@gmail.com>
2021-06-18 11:25:41 -0300

    chore: adjust eslint and add moduleAlias

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,14 @@
+import moduleAlias from 'module-alias';
+moduleAlias.addAlias('#', __dirname);
+
 import express from 'express';
-// rest of the code remains same
+import file from '#/file';
+
 const app = express();
 
 const PORT = 8000;
 
-app.get('/', (req, res) => res.send('Express + TypeScript Server!!'));
+app.get('/', (req, res) => res.send(file));
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
