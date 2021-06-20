174714d6c3a4e1805ed1f04a125cf0ab1402ebfd
Carlos Eustáquio <caeu227@gmail.com>
2021-06-20 02:02:12 -0300

    chore: add lint-staged to frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-use-before-define
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import {App} from '#/app';
 import reportWebVitals from '#/reportWebVitals';
 
