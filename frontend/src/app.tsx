baf9ad3fcfeeead3d4f60527a1f0529d405bf48d
Carlos Eustáquio <caeu227@gmail.com>
2021-06-20 13:26:28 -0300

    feat: add api_url env variable

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,10 +2,11 @@ import axios from 'axios';
 import {useEffect} from 'react';
 
 import {Text} from '#/text';
+import {API_URL} from '#/settings';
 
 export const App = () => {
   const getUsers = async () => {
-    const response = await axios.get('http://localhost:8080/api/user');
+    const response = await axios.get(API_URL + '/user');
     console.log(response.data);
   };
 
